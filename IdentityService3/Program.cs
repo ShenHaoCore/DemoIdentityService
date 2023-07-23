7c8d094ed52cf5b1d9919a56877ccfe2d0334208
ShenHaoCore <ShenHaoCore@163.com>
2023-07-23 20:16:19 +0800

    修改界面

--- IdentityService3/Program.cs
+++ IdentityService3/Program.cs
@@ -16,24 +16,20 @@ builder.Services.AddAuthentication().AddGoogle("Google", options =>
     options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme; 
     options.ClientId = "test"; 
     options.ClientSecret = "test";
-}).AddOpenIdConnect("aad", "Sign-in with Azure AD", options =>
+}).AddOpenIdConnect("AAD", "Sign-in with Azure AD", options =>
 {
     options.Authority = "https://login.microsoftonline.com/common";
     options.ClientId = "https://leastprivilegelabs.onmicrosoft.com/38196330-e766-4051-ad10-14596c7e97d3";
-
     options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
     options.SignOutScheme = IdentityServerConstants.SignoutScheme;
-
     options.ResponseType = "id_token";
     options.CallbackPath = "/signin-aad";
     options.SignedOutCallbackPath = "/signout-callback-aad";
     options.RemoteSignOutPath = "/signout-aad";
-
     options.TokenValidationParameters = new TokenValidationParameters
     {
         ValidateIssuer = false,
         ValidAudience = "165b99fd-195f-4d93-a111-3e679246e6a9",
-
         NameClaimType = "name",
         RoleClaimType = "role"
     };
