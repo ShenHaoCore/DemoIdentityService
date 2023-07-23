666397b235e8594a12c61007aa48e227669cf0c6
ShenHaoCore <ShenHaoCore@163.com>
2023-07-23 18:32:45 +0800

    添加第三方登录

--- IdentityService3/Program.cs
+++ IdentityService3/Program.cs
@@ -1,4 +1,6 @@
+using IdentityServer4;
 using IdentityService3;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
@@ -8,6 +10,37 @@ identity.AddInMemoryApiScopes(Config.ApiScopes);
 identity.AddInMemoryApiResources(Config.ApiResources);
 identity.AddInMemoryClients(Config.Clients);
 identity.AddDeveloperSigningCredential();
+
+builder.Services.AddAuthentication().AddGoogle("Google", options =>
+{
+    options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme; 
+    options.ClientId = "test"; 
+    options.ClientSecret = "test";
+}).AddOpenIdConnect("aad", "Sign-in with Azure AD", options =>
+{
+    options.Authority = "https://login.microsoftonline.com/common";
+    options.ClientId = "https://leastprivilegelabs.onmicrosoft.com/38196330-e766-4051-ad10-14596c7e97d3";
+
+    options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+    options.SignOutScheme = IdentityServerConstants.SignoutScheme;
+
+    options.ResponseType = "id_token";
+    options.CallbackPath = "/signin-aad";
+    options.SignedOutCallbackPath = "/signout-callback-aad";
+    options.RemoteSignOutPath = "/signout-aad";
+
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = false,
+        ValidAudience = "165b99fd-195f-4d93-a111-3e679246e6a9",
+
+        NameClaimType = "name",
+        RoleClaimType = "role"
+    };
+}).AddLocalApi(options =>
+{
+    options.ExpectedScope = "api";
+});
 
 var app = builder.Build();
 app.UseStaticFiles();
